cf203457abf8c3c714dd1ddaa9ee4576ec40f135
tom.debek@gmail.com <tom.debek@gmail.com>
2021-05-31 20:46:42 +0200

    Display my products on the page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  has_one_attached :main_image
+
+  def main_image_path
+    main_image.attached? ? main_image : 'http://placehold.it/700x400'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Product.delete_all
+10.times do
+  Product.create!(
+    name: Faker::Game.title,
+    price: Faker::Commerce.price
+  )
+end
