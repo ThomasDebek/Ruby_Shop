de91a2fecc3a0050f5b11e63603e33eb5e40cadc
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-01 11:43:41 +0200

    Add model category and add reference categroy to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   has_one_attached :main_image
+  belongs_to :category
 
   def main_image_path
     main_image.attached? ? main_image : 'http://placehold.it/700x400'
