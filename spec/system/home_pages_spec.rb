4696e2b93250731a2d26710e018dd013d1549206
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-01 11:02:27 +0200

    Tests

--- spec/system/home_pages_spec.rb
+++ spec/system/home_pages_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe "HomePages", type: :system do
   before do
     driven_by(:rack_test)
+    Product.create(name:"Tomasz", price: 2)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "views all prodcuts" do
+    visit root_path
+    expect(page).to have_text("Tomasz")
+    expect(page).to have_text("2")
+    expect(page).to have_selector "h4.card-title"
+  end
 end
