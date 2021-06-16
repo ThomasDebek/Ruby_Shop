649e1ad88c78e638afc4896033328cdc582cae7f
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-16 12:53:16 +0200

    feat: add new product from administrator dashboard

--- app/controllers/administrator/products_controller.rb
+++ app/controllers/administrator/products_controller.rb
@@ -6,4 +6,24 @@ class Administrator::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to administrator_products_path, notice: 'Product added successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+  end
+
 end
