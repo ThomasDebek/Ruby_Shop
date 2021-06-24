96dcdff4aa09eba7332242e11a9856fdaf74acd3
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-24 19:55:10 +0200

    test: add product_systems_spec

---
+++ spec/system/products/add_product_systems_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+RSpec.describe 'Adding Product', type: :system do
+  context 'when logged in as admin' do
+    let!(:administrator) { create(:administrator) }
+
+    before do
+      login_as(administrator, scope: :administrator)
+      visit administrator_products_path
+    end
+
+    context 'with valid product attributes' do
+      let(:product_params) { attributes_for(:product) }
+
+      before do
+        find('#add-product-button').click
+        fill_in 'product_name', with: product_params[:name]
+        fill_in 'product_price', with: product_params[:price]
+        click_button 'Create Product'
+      end
+
+      it 'is successful' do
+        expect(page).to have_content 'Product added successfully'
+      end
+
+      it 'shows new product on product_index' do
+        expect(page).to have_content product_params[:name]
+      end
+
+      it 'shows new product on home page' do
+        visit '/'
+        expect(page).to have_content product_params[:name]
+      end
+    end
+
+    context 'with missing price' do
+      let(:product_name) { 'product_with_no_price' }
+      before  do
+        find('#add-product-button').click
+        fill_in 'product_name', with: product_name
+        click_button 'Create Product'
+      end
+
+      it 'displays missing price error' do
+        expect(page).to have_content 'Price can\'t be blank'
+      end
+
+      it 'does not add new product' do
+        visit '/'
+        expect(page).to have_no_content product_name
+      end
+    end
+
+    context 'with missing name'  do
+      let(:product_price) { 1 }
+      before  do
+        find('#add-product-button').click
+        fill_in 'product_price', with: product_price
+        click_button 'Create Product'
+      end
+
+      it 'displays missing name error' do
+        expect(page).to have_content 'Name can\'t be blank'
+      end
+
+      it 'does not add new product' do
+        visit '/'
+        expect(page).to have_no_content 'Price can\'t be blank'
+      end
+
+    end
+  end
+
+
+  context 'when logged in as user' do
+    before do
+      login_as(create(:user), scope: :user)
+      visit administrator_products_path
+    end
+
+    it 'redirects to root path' do
+      expect(page).to have_current_path(root_path)
+    end
+
+    it 'displays unauthorized warning' do
+      expect(page).to have_content('You are not authorized.')
+    end
+  end
+
+  context 'when not logged in' do
+    before do
+      visit administrator_products_path
+    end
+
+    it 'redirects to root path' do
+      expect(page).to have_current_path(root_path)
+    end
+
+    it 'displays unauthorized warning' do
+      expect(page).to have_content('You are not authorized.')
+    end
+  end
+
+end
