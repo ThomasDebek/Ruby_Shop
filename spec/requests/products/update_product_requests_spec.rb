652f089005e72cba867be085f95e03f88520e42b
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-24 18:47:07 +0200

    test: add update_prodcut_requests_spec

--- spec/requests/products/update_product_requests_spec.rb
+++ spec/requests/products/update_product_requests_spec.rb
@@ -1,10 +1,84 @@
 require 'rails_helper'
 
-RSpec.describe "Products::UpdateProductRequests", type: :request do
-  describe "GET /products/update_product_requests" do
-    it "works! (now write some real specs)" do
-      get products_update_product_requests_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'PATH /administrator/products/:id', type: :request do
+  let!(:product) { create(:product) }
+  let(:new_name) { 'new_product_name' }
+  let(:new_price) { 123_456 }
+
+  context 'when logged in as administrator' do
+    let!(:administrator) { create(:administrator) }
+
+    before do
+      login_as(administrator, scope: :administrator)
+    end
+
+    context 'with valid parameters' do
+      before do
+        patch administrator_product_url(product), params: { product:
+                                                              { name: new_name, price: new_price } }
+      end
+
+      it 'update product\'s name' do
+        product.reload
+        expect(product.name).to eq(new_name)
+      end
+
+      it 'update product\'s name' do
+        product.reload
+        expect(product.price).to eq(new_price)
+      end
+    end
+
+    context 'with empty product\'s price' do
+      it 'does not update a product ' do
+        patch administrator_product_url(product), params: { product: { price: '' } }
+        product.reload
+        expect(product.price).not_to eq('')
+      end
+    end
+
+    context 'with empty product\s name' do
+      it 'does not update a product' do
+        patch administrator_product_url(product), params: { product: { name: '' } }
+        product.reload
+        expect(product.name).not_to eq('')
+      end
     end
   end
+
+  context 'when logged in as user' do
+    let!(:user) { create(:user) }
+    before do
+      login_as(user, scope: :user)
+    end
+
+    context 'with valid parameters' do
+      before do
+        patch administrator_product_url(product), params: { product: { name: new_name, price: new_price } }
+      end
+      it 'does not update product\s price' do
+        product.reload
+        expect(product.price).not_to eq(new_price)
+      end
+    end
+  end
+
+  context 'when not logged in'  do
+    context 'with valid parmaeters'  do
+      before  do
+        patch administrator_product_url(product), params: { product: {name:new_name, price:new_price }}
+      end
+
+      it 'does not update product\s name' do
+        product.reload
+        expect(product.name).not_to eq(new_name)
+      end
+
+      it 'does not update product\'s price' do
+        product.reload
+        expect(product.price).not_to eq(new_price)
+      end
+    end
+  end
+
 end
