79e48c49d848559ae0682125e5e236a0353b0864
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-28 11:44:51 +0200

    feat: fix controller

--- app/controllers/administrator/products_controller.rb
+++ app/controllers/administrator/products_controller.rb
@@ -1,50 +1,49 @@
 # frozen_string_literal: true
 
-module Administrator
-  class ProductsController < ApplicationController
-    layout 'administrator'
-    before_action :authenticate_administrator!
+class Administrator::ProductsController < ApplicationController
+  layout 'administrator'
+  before_action :authenticate_administrator!
 
-    def index
-      @products = Product.includes([:category]).includes([:brand]).all
-    end
+  def index
+    @products = Product.includes([:category]).includes([:brand]).all
+  end
 
-    def new
-      @product = Product.new
-    end
+  def new
+    @product = Product.new
+  end
 
-    def create
-      @product = Product.new(product_params)
-      if @product.save
-        redirect_to administrator_products_path, notice: 'Product added successfully'
-      else
-        render :new
-      end
-    end
-
-    def edit
-      @product = Product.find(params[:id])
-    end
-
-    def update
-      @product = Product.find(params[:id])
-      if @product.update(product_params)
-        redirect_to administrator_products_path, notice: 'Product edited successfully'
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @product = Product.find(params[:id])
-      @product.destroy
-      redirect_to administrator_products_path, notice:  'Product deleted successfully'
-    end
-
-    private
-
-    def product_params
-      params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to administrator_products_path, notice: 'Product added successfully'
+    else
+      render :new
     end
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to administrator_products_path, notice: 'Product edited successfully'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to administrator_products_path, notice: 'Product deleted successfully'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+  end
 end
+
