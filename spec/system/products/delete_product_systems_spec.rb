cd5dd958364620632c9667ec1dc962303001491e
tom.debek@gmail.com <tom.debek@gmail.com>
2021-06-28 12:58:23 +0200

    test: add delete_products_system_speces

--- spec/system/products/delete_product_systems_spec.rb
+++ spec/system/products/delete_product_systems_spec.rb
@@ -1,9 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Products::DeleteProductSystems", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+RSpec.describe 'Deleting Product', type: :system do
+  context 'when logged in as administrator' do
+    let!(:administrator) { create(:administrator) }
+    let!(:product) { create(:product) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    before do
+      login_as(administrator, scope: :administrator)
+      visit administrator_products_path
+    end
+
+    context 'when deleting product' do
+      before do
+        find("#delete-product-#{product.id}-button").click
+      end
+
+      it 'deletes product successfully' do
+        expect(page).to have_content 'Product deleted successfully'
+      end
+
+      it 'removes product from product index' do
+        expect(page).not_to have_content product.name
+      end
+    end
+  end
 end
