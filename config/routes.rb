2f4f5d64e2753606658106443f54687da8c211da
tom.debek@gmail.com <tom.debek@gmail.com>
2021-07-26 16:56:24 +0200

    feat: add order creation at checkout

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,12 @@ Rails.application.routes.draw do
   end
 
   resources :products, only: %i[show]
+  resources :orders, only: %i[create]
 
   resources :carts, only: %i[show create destroy]
+
   get    'cart', to: 'carts#show'
   delete 'cart', to: 'carts#destroy'
   post   'cart', to: 'carts#add', as: 'add_to_cart'
-
 
 end
